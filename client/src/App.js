8be463c7af5e9b2fcf0fbd47edba384e9c330095
Jonathan <jmoshev@gmail.com>
2019-06-03 22:31:45 +0300

    Playing with react-bootsrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Form, Button } from 'react-bootstrap';
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -26,28 +28,36 @@ class App extends Component {
         const { fetchWorkers, props } = this;
         const { workersAge, workersSalary, workersLoading, workersError, workers } = props;
         return (
-                <div className="section section-hero bg-gray">
-                    <div className="grid-hero container grid-lg text-center" id="overview">
+          <main role = "main" className = "container" >
 
-                        <h1>Get Workers</h1>
-                        <div className="columns">
-                            <div className="column col-4 col-xs-12"></div>
-                            <WorkersError workersError={workersError}/>
-                            <WorkersFetch
-                                workersError={workersError}
-                                workersLoading={workersLoading}
-                                fetchWorkers={fetchWorkers}
-                            />
-                            <WorkersDetails
-                                hide={workersError || workersLoading || workers.length === 0}
-                                workersError={workersError}
-                                workersSalary={workersSalary}
-                                workersAge={workersAge}
-                            />
-                            <div className="column col-4 col-xs-12"></div>
-                        </div>
-                    </div>
-                </div>
+            <div class = "starter-template">
+            <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group controlId="formBasicChecbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+
+
+            </div>
+
+            </main>
+
+
 
         );
     }
